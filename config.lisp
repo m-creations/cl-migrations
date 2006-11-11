ac8eb6ba33083b229aef48f754717de6f46a6e51
vkanakala <vkanakala@common-lisp.net>
2006-11-11 16:40:33 +0200

    Initial import

---
+++ config.lisp
@@ -0,0 +1,119 @@
+;; CL-MIGRATIONS is released under the MIT Licence: 
+;;
+;; Copyright (c) <2006> <Vamsee Krishna Kanakala>
+;;
+;; Permission is hereby granted, free of charge, to any person 
+;; obtaining a copy of this software and associated documentation 
+;; files (the "Software"), to deal in the Software without restriction, 
+;; including without limitation the rights to use, copy, modify, merge,
+;; publish, distribute, sublicense, and/or sell copies of the Software,
+;; and to permit persons to whom the Software is furnished to do so, 
+;; subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+;; OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+;; ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+;; SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+(in-package :cl-migrations)
+
+(defvar *config-pathname* (make-pathname :name ".migrate" 
+					 :type "conf" 
+					 :defaults (user-homedir-pathname)))
+(defvar *migration-dir* nil)
+
+(defvar +all-db-types+
+  '(:postgresql :postgresql-socket :mysql :sqlite :sqlite3 :odbc :oracle
+    #+allegro :aodbc))
+
+;; You can change your schema table name here.
+(defvar *schema-table-name* "schema_info")
+
+(defclass conn-specs ()
+  ((aodbc :accessor aodbc-spec :initform nil)
+   (mysql :accessor mysql-spec :initform nil)
+   (postgresql :accessor postgresql-spec :initform nil)
+   (postgresql-socket :accessor postgresql-socket-spec :initform nil)
+   (sqlite :accessor sqlite-spec :initform nil)
+   (sqlite3 :accessor sqlite3-spec :initform nil)
+   (odbc :accessor odbc-spec :initform nil)
+   (oracle :accessor oracle-spec :initform nil))
+  (:documentation "Connection specs for CLSQL for different databases"))
+
+(defun read-specs (&optional (path *config-pathname*))
+  "Read database and migrations directory specs and set respective variables."
+  (if (probe-file path)
+      (with-open-file (stream path :direction :input)
+	(let ((spec (make-instance 'conn-specs))
+	      (specs (read stream)))
+	  (setf (slot-value spec 
+			    (intern (symbol-name (first (first specs)))
+				    (find-package '#:cl-migrations)))
+		(second (first specs)))
+	  (setf *migration-dir* (first (second (second specs))))
+	  spec))))
+
+(defun spec-fn (db-type)
+  (intern (concatenate 'string (symbol-name db-type)
+		       (symbol-name '#:-spec))
+	  (find-package '#:cl-migrations)))
+
+(defun db-type-spec (db-type spec)
+  (funcall (spec-fn db-type) spec))
+
+(defun init-connection (db-type spec &optional clean)
+  "Connect to the database; Truncate database if clean flag is set."
+  (clsql:connect spec
+		 :database-type db-type
+		 :make-default t
+		 :if-exists :old)
+  ;; Ensure database is empty
+  (when clean
+    (warn "Clean flag set, truncating database ~S" (second spec))
+    (truncate-database :database *default-database*))
+  *default-database*)
+
+(defun load-necessary-systems (spec &optional clean)
+  "Load the matching database type"
+  (dolist (db-type +all-db-types+)
+    (when (db-type-spec db-type spec)
+      (clsql-sys:initialize-database-type :database-type db-type)
+      (init-connection db-type (db-type-spec db-type spec) clean)
+      (return))))
+
+(defun create-schema-table ()
+  "Check for schema table and create if it does not exist."
+  (if (table-exists-p *schema-table-name*)
+      (format t "Schema table exists.")
+      (progn 
+	(format t "Schema table doesn't exist, creating.")
+	(create-table *schema-table-name* '((version integer)))
+	;;Initialize the version number to 0.
+	(insert-records :into *schema-table-name*
+			:attributes '(version)
+			:values '(0)))))
+
+(defun init-config (&optional clean)
+  "Load database configuration and create a connection if none exists."
+  (let ((spec (read-specs))
+	(error-count 0))
+    (unless spec
+      (warn "Exiting - .migrate.conf not found in the home directory")
+      (incf error-count)
+      (return-from init-config :skipped))
+    (load-necessary-systems spec clean)
+    (unless *default-database*
+      (warn "Unable to connect to database. Please check your CLSQL setup.")
+      (incf error-count)
+      (return-from init-config :skipped))
+    ;;The spec works, so we create the schema table
+    (create-schema-table)
+    (format t "~%Database intialized.")
+    (zerop error-count)))
---
+++ migrate.lisp
@@ -0,0 +1,144 @@
+;; CL-MIGRATIONS is released under the MIT Licence: 
+;;
+;; Copyright (c) <2006> <Vamsee Krishna Kanakala>
+;;
+;; Permission is hereby granted, free of charge, to any person 
+;; obtaining a copy of this software and associated documentation 
+;; files (the "Software"), to deal in the Software without restriction, 
+;; including without limitation the rights to use, copy, modify, merge,
+;; publish, distribute, sublicense, and/or sell copies of the Software,
+;; and to permit persons to whom the Software is furnished to do so, 
+;; subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+;; OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+;; ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+;; SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+(in-package :cl-migrations)
+
+(defun get-db-version ()
+  (first (select 'version
+	  :from *schema-table-name*
+	  :flatp t
+	  :field-names nil)))
+
+#.(locally-enable-sql-reader-syntax)
+(defun incf-db-version ()
+  (let ((db-version (get-db-version)))
+    (update-records (make-symbol *schema-table-name*)
+		    :av-pairs `((version ,(1+ db-version)))
+		    :where [= [version] db-version])))
+(defun decf-db-version ()
+  (let ((db-version (get-db-version)))
+    (update-records (make-symbol *schema-table-name*)
+		    :av-pairs `((version ,(1- db-version)))
+		    :where [= [version] db-version])))
+#.(restore-sql-reader-syntax-state)
+
+(defun get-migration-number (file)
+  (parse-integer 
+   (subseq (file-namestring file) 0 (search "-" (file-namestring file)))))
+
+(defun compare-files (file1 file2)
+  (string< (file-namestring file1) (file-namestring file2)))
+
+(defun get-migration-files ()
+  "Get all files from the migrations directory, and sort them numerically."
+  (unless *migration-dir*
+    (warn "Migrations directory not set properly. Please check your config file.")
+    (return-from get-migration-files :skipped))
+  (let ((files (directory (make-pathname :name :wild
+					 :type "lisp"
+					 :defaults *migration-dir*))))
+    (sort files #'compare-files)))
+
+(defun get-latest-migration ()
+  "Get the version of latest migration available."
+  (let ((files-list (get-migration-files))
+	(counter 0))
+    (dolist (file files-list counter)
+      (unless (equal (incf counter) (get-migration-number file))
+	(warn "Migration #~S is missing!" counter)
+	(return)))))
+	 
+(defun generate (name)
+  "Generate an empty migration file with an assigned version number."
+  (unless *migration-dir*
+    (warn "Migrations directory not set properly. Please check your config file.")
+    (return-from generate :skipped))
+  (let ((version (get-latest-migration)))
+    (unless version
+      (return-from generate :skipped))
+    (let* ((file-name (make-pathname 
+		     :name (concatenate 
+			    'string 
+			    (write-to-string (incf version)) "-" name)
+		     :type "lisp"
+		     :defaults *migration-dir*))
+	   (file-name-str (file-namestring file-name)))
+      (with-open-file (stream file-name 
+			      :direction :output 
+			      :if-does-not-exist :create)
+	(write-line (concatenate 'string ";; " file-name-str) stream)
+	(with-standard-io-syntax 
+	  (prin1 '(:up () :down ()) stream)))
+      file-name-str)))
+  
+(defun get-file-range (from to)
+  "Get the migration files within the given range, in ascending order."
+  (remove-if #'(lambda (file)
+		 (or (< (get-migration-number file) from)
+		     (> (get-migration-number file) to))) (get-migration-files)))
+  
+(defun select-migration-files (db-version mig-version)
+  "Return migration files in ascending or descending order based on 
+   whether migrating up or down respectively."
+    (if (> mig-version db-version)
+	(get-file-range (1+ db-version) mig-version)
+	(reverse (get-file-range (1+ mig-version) db-version))))
+
+(defun exec-migrations (db-version mig-version)
+  "Excute all the migrations within the given range in a transaction."
+  (let ((files (select-migration-files db-version mig-version)))
+    (with-transaction ()
+      (dolist (file files (length files))
+	(format t "~%~%Migration #~S:" (get-migration-number file))
+	(with-open-file (stream file)
+	  (with-standard-io-syntax
+	    (let* ((migration (read stream))
+		   (ddl (if (> mig-version db-version)
+			    (getf migration :up)
+			  (getf migration :down))))
+	      (dolist (statement ddl)
+		(format t "~%EXEC: ~S" (first statement))
+		(execute-command (first statement))))))
+	(if (> mig-version db-version)
+	    (incf-db-version)
+	    (decf-db-version))))))
+  
+(defun migrate (&key version)
+  "Initiate migration procedure."
+  (when (init-config)
+    (unwind-protect
+	 (let ((db-version (get-db-version))
+	       (mig-version (get-latest-migration)))
+	   (unless mig-version
+	     (return-from migrate :skipped))
+	   (unless version
+	     (setf version mig-version))
+	   (cond ((> version mig-version) 
+		  (warn "Migration not present.")
+		  (return-from migrate :skipped))
+		 ((equal db-version version) (warn "Nothing to migrate."))
+		 (t (format t "~%~%-----------------~%Total migrations: ~S" 
+			    (exec-migrations db-version version)))))
+      (disconnect))))
+  
